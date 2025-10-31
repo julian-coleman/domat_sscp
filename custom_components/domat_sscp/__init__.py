3c62223bbb5ff2b6d103d8c15f508f995fe57ad8
Julian Coleman <jdc@coris.org.uk>
2025-10-31 17:51:33 +0100

    Make the coordinator run, although with dummy data.

--- custom_components/domat_sscp/__init__.py
+++ custom_components/domat_sscp/__init__.py
@@ -7,18 +7,23 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .coordinator import DomatSSCPConfigEntry, DomatSSCPCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 _PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(
+    hass: HomeAssistant, config_entry: DomatSSCPConfigEntry
+) -> bool:
     """Set up Domat SSCP from a config entry."""
 
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
+    coordinator = DomatSSCPCoordinator(hass, config_entry)
+    await coordinator.async_config_entry_first_refresh()
+    if not coordinator.data:
+        raise ConfigEntryNotReady
 
     # await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
 
@@ -26,15 +31,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
-    """Update setup listener."""
-
-    await hass.config_entries.async_reload(entry.entry_id)
-
-    _LOGGER.info("Settings updated")
-
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    #    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
+    # return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
     return True
--- custom_components/domat_sscp/coordinator.py
+++ custom_components/domat_sscp/coordinator.py
@@ -9,6 +9,7 @@ from typing import Any
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_IP_ADDRESS,
+    CONF_PASSWORD,
     CONF_PORT,
     CONF_SCAN_INTERVAL,
     CONF_USERNAME,
@@ -17,14 +18,18 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import (
+from .sscp_connection import sscp_connection
+from .sscp_const import (
     CONF_CONNECTION_NAME,
     CONF_SSCP_ADDRESS,
-    DEFAULT_POLL_INTERVAL,
+    DEFAULT_SCAN_INTERVAL,
     DOMAIN,
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+
+type DomatSSCPConfigEntry = ConfigEntry[list[DomatSSCPCoordinator]]
 
 
 class DomatSSCPCoordinator(DataUpdateCoordinator):
@@ -35,22 +40,61 @@ class DomatSSCPCoordinator(DataUpdateCoordinator):
     def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
         """Initialize coordinator."""
 
-        self.ip = config_entry.data[CONF_IP_ADDRESS]
+        # From config flow
+        self.connection_name = config_entry.data[CONF_CONNECTION_NAME]
+        self.ip_address = config_entry.data[CONF_IP_ADDRESS]
         self.port = config_entry.data[CONF_PORT]
-        self.user = config_entry.data[CONF_USERNAME]
-        self.addr = config_entry.data[CONF_SSCP_ADDRESS]
+        self.username = config_entry.data[CONF_USERNAME]
+        self.password = config_entry.data[CONF_PASSWORD]
+        self.sscp_address = config_entry.data[CONF_SSCP_ADDRESS]
 
+        # From options
         self.poll_interval = config_entry.options.get(
-            CONF_SCAN_INTERVAL, DEFAULT_POLL_INTERVAL
+            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
         )
 
         super().__init__(
             hass,
             logger=_LOGGER,
             config_entry=config_entry,
-            name=f"{DOMAIN} {config_entry[CONF_CONNECTION_NAME]} ({config_entry.unique_id})",
+            name=f"{DOMAIN} {config_entry.data[CONF_CONNECTION_NAME]} ({config_entry.unique_id})",
             update_interval=timedelta(seconds=self.poll_interval),
         )
 
     async def _async_update_data(self):
         """Fetch data from the server and convert it to the right format."""
+
+        _LOGGER.error("Updating %s", self.connection_name)
+        try:
+            conn = sscp_connection(
+                name=self.connection_name,
+                ip_address=self.ip_address,
+                port=self.port,
+                user_name=self.username,
+                password=self.password,
+                sscp_address=self.sscp_address,
+            )
+        except ValueError as error:
+            _LOGGER.error("Could not create a connection object")
+            raise UpdateFailed from error
+
+        try:
+            await conn.login()
+            if conn.socket is None:
+                _LOGGER.debug("Login failed")
+                _LOGGER.error("Login failed")
+                raise ConfigEntryAuthFailed from None
+        except TimeoutError:
+            _LOGGER.debug("Login timeout")
+            _LOGGER.error("Login timeout")
+            raise ConfigEntryAuthFailed from None
+        except (ValueError, OSError):
+            _LOGGER.debug("Login connection error")
+            _LOGGER.error("Login connection error")
+            raise UpdateFailed from None
+
+        data = ["dummy", "dummy"]
+
+        await conn.logout()
+        _LOGGER.error("All OK")
+        return data
