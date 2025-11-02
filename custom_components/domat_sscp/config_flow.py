f8e00094d2d99b1ccd51fc3b1b130dbc632914d4
Julian Coleman <jdc@coris.org.uk>
2025-11-02 20:58:19 +0100

    Add initial options flow for devices.

--- custom_components/domat_sscp/config_flow.py
+++ custom_components/domat_sscp/config_flow.py
@@ -9,8 +9,11 @@ import voluptuous as vol
 
 from homeassistant.config_entries import (
     SOURCE_RECONFIGURE,
+    ConfigEntry,
     ConfigFlow,
     ConfigFlowResult,
+    OptionsFlow,
+    #    OptionsFlowWithReload,
 )
 from homeassistant.const import (
     CONF_IP_ADDRESS,
@@ -19,44 +22,68 @@ from homeassistant.const import (
     #    CONF_SCAN_INTERVAL,
     CONF_USERNAME,
 )
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.data_entry_flow import section
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import selector
+import homeassistant.helpers.entity_registry as er
+from homeassistant.helpers.selector import (
+    NumberSelector,
+    NumberSelectorConfig,
+    NumberSelectorMode,
+)
 
-from .sscp_connection import sscp_connection
-from .sscp_const import (
+from .const import (
     CONF_CONNECTION_NAME,
     CONF_SSCP_ADDRESS,
     #    DEFAULT_SCAN_INTERVAL,
     DEFAULT_SSCP_ADDRESS,
     DEFAULT_SSCP_PORT,
     DOMAIN,
+    OPT_CALORIMETER_COLD,
+    OPT_CALORIMETER_HOT,
+    OPT_CO2,
+    OPT_DESCRIPTION,
+    OPT_HUMIDITY,
+    OPT_MAXIMUM,
+    OPT_MINIMUM,
+    OPT_METER_ELECTRICITY,
+    OPT_TEMPERATURE,
+    OPT_UID,
+    OPT_VENTILATOR_IN,
+    OPT_VENTILATOR_OUT,
+    OPT_VENTILATOR_ERROR,
+    OPT_VENTILATOR_FLOW,
+    OPT_VENTILATOR_FLOW_MINIMUM,
+    OPT_VENTILATOR_FLOW_MAXIMUM,
+    OPT_WATER_COLD,
+    OPT_WATER_HOT,
 )
+from .sscp_connection import sscp_connection
 
 _LOGGER = logging.getLogger(__name__)
 
+# Config flow schemas
 _PORT_SELECTOR = vol.All(
-    selector.NumberSelector(
-        selector.NumberSelectorConfig(
-            min=1, max=65535, mode=selector.NumberSelectorMode.BOX
-        ),
+    NumberSelector(
+        NumberSelectorConfig(min=1, max=65535, mode=NumberSelectorMode.BOX),
     ),
     vol.Coerce(int),
 )
 _ADDR_SELECTOR = vol.All(
-    selector.NumberSelector(
-        selector.NumberSelectorConfig(
-            min=1, max=255, mode=selector.NumberSelectorMode.BOX
-        ),
+    NumberSelector(
+        NumberSelectorConfig(min=1, max=255, mode=NumberSelectorMode.BOX),
     ),
     vol.Coerce(int),
 )
+
+# Options flow schemas
+_DEVICE_MENU = ["temp_hum", "energy", "air"]
 
 
 def get_user_schema(
     input_data: dict[str, Any] | None = None,
 ) -> vol.Schema:
-    """Return a schema with defaults based on the step and user input."""
+    """Return a config flow schema with defaults based on the step and user input."""
 
     # Fill in defaults from initial defaults or input
     if input_data is None:
@@ -88,7 +115,53 @@ def get_user_schema(
     )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+def get_temp_hum_schema(
+    input_data: dict[str, Any] | None = None,
+) -> vol.Schema:
+    """Return a temperature/humidity flow schema with defaults based on the user input."""
+
+    # Fill in defaults from input or initial defaults
+    if input_data is None:
+        default_description = ""
+        default_temperature_uid = 0
+        default_humidity_uid = 0
+    else:
+        default_description = input_data.get(OPT_DESCRIPTION, "")
+        default_temperature_uid = 0
+        temperature = input_data.get(OPT_TEMPERATURE, None)
+        if temperature is not None:
+            default_temperature_uid = temperature.get(OPT_UID, default_temperature_uid)
+        default_humidity_uid = 0
+        humidity = input_data.get(OPT_HUMIDITY, None)
+        if humidity is not None:
+            default_humidity_uid = humidity.get(OPT_UID, default_humidity_uid)
+
+    schema = vol.Schema(
+        {
+            vol.Required(OPT_DESCRIPTION, default=default_description): str,
+            vol.Required(OPT_TEMPERATURE): section(
+                vol.Schema(
+                    {
+                        vol.Optional(OPT_UID, default=default_temperature_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_HUMIDITY): section(
+                vol.Schema(
+                    {
+                        vol.Optional(OPT_UID, default=default_humidity_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+        }
+    )
+
+    return schema
+
+
+async def validate_config(data: dict[str, Any]) -> dict[str, Any]:
     """Validate that the user input allows us to connect using the values provided by the user."""
 
     try:
@@ -126,6 +199,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
         _LOGGER.debug("Info failed")
         raise CannotConnect from None
     if serial is None:
+        # TODO Always add the username and SSCP address
         _LOGGER.error("No serial number for %s", data[CONF_CONNECTION_NAME])
         serial = CONF_USERNAME + "-" + CONF_SSCP_ADDRESS
     _LOGGER.info("Using unique ID: %s", serial)
@@ -148,12 +222,11 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
 
         errors: dict[str, str] = {}
-
         if self.source == SOURCE_RECONFIGURE:
             entry = self._get_reconfigure_entry()
             input_data = entry.data
             step = "reconfigure"
-        else:
+        else:  # user
             input_data = None
             step = "user"
 
@@ -167,7 +240,7 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
 
         # Validate the user input and create an entry
         try:
-            info = await validate_input(self.hass, user_input)
+            info = await validate_config(user_input)
         except CannotConnect:
             errors["base"] = "cannot_connect"
         except Timeout:
@@ -185,10 +258,10 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
                     entry,
                     data_updates=user_input,
                 )
-            else:
-                await self.async_set_unique_id(str(info["serial"]))
-                self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=info["title"], data=user_input)
+            # user
+            await self.async_set_unique_id(str(info["serial"]))
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title=info["title"], data=user_input)
 
         # There was some validation problem - previous input as defaults
         return self.async_show_form(
@@ -202,6 +275,240 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
     ) -> ConfigFlowResult:
         """Handle reconfiguration."""
         return await self.async_step_user(user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
+        """Setup an options flow for this handler."""
+        return DomatSSCPOptionsFlowHandler(config_entry)
+
+
+class DomatSSCPOptionsFlowHandler(OptionsFlow):
+    """Handles options flow for Domat SSCP."""
+
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options for Domat SSCP."""
+
+        return self.async_show_menu(step_id="init", menu_options=_DEVICE_MENU)
+
+    async def async_step_temp_hum(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options for a temperature/humidity device."""
+
+        errors: dict[str, str] = {}
+        step = "temp_hum"
+        schema = get_temp_hum_schema(user_input)
+
+        if user_input is None:
+            return self.async_show_form(step_id=step, data_schema=schema, errors=errors)
+
+        _LOGGER.error("User input: %s", user_input)
+        temperature = user_input.get(OPT_TEMPERATURE, None)
+        if temperature is not None:
+            temperature_uid = temperature.get(OPT_UID, 0)
+            if temperature_uid != 0:
+                _LOGGER.error("Temperature: %s %s %s", temperature_uid, 4, 0)
+        humidity = user_input.get(OPT_HUMIDITY, None)
+        if humidity is not None:
+            humidity_uid = humidity.get(OPT_UID, 0)
+            if humidity_uid != 0:
+                _LOGGER.error("Humidity: %s %s %s", humidity_uid, 4, 0)
+
+        # Validate the user input and create an entry
+#        try:
+#            error_variables = await validate_variables(variables)
+#        except CannotConnect:
+#            errors["base"] = "cannot_connect"
+#        except Timeout:
+#            errors["base"] = "timeout_connect"
+#        except InvalidAuth:
+#            errors["base"] = "invalid_auth"
+#        except Exception:
+#            _LOGGER.exception("Unexpected exception")
+#            errors["base"] = "unknown"
+#        else:
+
+        error_variables_string = str(temperature_uid)
+        description_placeholders = {"variables": error_variables_string}
+        errors["base"] = "variable_length"
+        # return self.async_create_entry(data=user_input)
+
+        # There was some validation problem - previous input as defaults
+        return self.async_show_form(
+            step_id=step,
+            data_schema=schema,
+            errors=errors,
+            description_placeholders=description_placeholders,
+        )
+
+    async def async_step_energy(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options for an energy usage device."""
+
+        errors: dict[str, str] = {}
+        step = "energy"
+        schema = vol.Schema(
+            {
+                vol.Required(OPT_METER_ELECTRICITY): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_WATER_COLD): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_WATER_HOT): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_CALORIMETER_COLD): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_CALORIMETER_HOT): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+            }
+        )
+
+        if user_input is None:
+            return self.async_show_form(
+                step_id="energy", data_schema=schema, errors=errors
+            )
+
+        # Validate the user input and create an entry
+        try:
+            error_variables = await validate_variables(variables)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except Timeout:
+            errors["base"] = "timeout_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+#        else:
+
+        # There was some validation problem - previous input as defaults
+        return self.async_show_form(
+            step_id=step,
+            data_schema=schema,
+            errors=errors,
+        )
+
+    async def async_step_air(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manage the options for an air recuperation device."""
+
+        errors: dict[str, str] = {}
+        step = "air"
+
+        schema = vol.Schema(
+            {
+                vol.Required(OPT_VENTILATOR_IN): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_VENTILATOR_OUT): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_CO2): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Required(OPT_VENTILATOR_ERROR): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+                vol.Optional(OPT_VENTILATOR_FLOW): section(
+                    vol.Schema(
+                        {
+                            vol.Optional(OPT_UID, default=0): int,
+                            vol.Optional(
+                                OPT_MINIMUM, default=OPT_VENTILATOR_FLOW_MINIMUM
+                            ): int,
+                            vol.Optional(
+                                OPT_MAXIMUM, default=OPT_VENTILATOR_FLOW_MAXIMUM
+                            ): int,
+                        }
+                    ),
+                    {"collapsed": False},
+                ),
+            }
+        )
+
+        if user_input is None:
+            return self.async_show_form(
+                step_id="air", data_schema=schema, errors=errors
+            )
+
+        # Validate the user input and create an entry
+        try:
+            error_variables = await validate_variables(variables)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except Timeout:
+            errors["base"] = "timeout_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+#        else:
+
+        # There was some validation problem - previous input as defaults
+        return self.async_show_form(
+            step_id=step,
+            data_schema=schema,
+            errors=errors,
+        )
 
 
 class CannotConnect(HomeAssistantError):
--- custom_components/domat_sscp/const.py
+++ custom_components/domat_sscp/const.py
@@ -11,3 +11,27 @@ CONF_SSCP_ADDRESS = "sscp_address"
 DEFAULT_SCAN_INTERVAL = 60
 DEFAULT_SSCP_PORT = 12346
 DEFAULT_SSCP_ADDRESS = 1
+
+# Options flow constants
+OPT_DESCRIPTION = "description"
+OPT_TEMPERATURE = "temperature"
+OPT_HUMIDITY = "humidity"
+OPT_METER_ELECTRICITY = "meter_electricity"
+OPT_WATER_COLD = "water_cold"
+OPT_WATER_HOT = "water_hot"
+OPT_CALORIMETER_COLD = "calorimeter_cold"
+OPT_CALORIMETER_HOT = "calorimeter_hot"
+OPT_VENTILATOR_IN = "ventilator_in"
+OPT_VENTILATOR_OUT = "ventilator_out"
+OPT_CO2 = "cotwo"
+OPT_VENTILATOR_ERROR = "ventilator_error"
+OPT_VENTILATOR_FLOW = "ventilator_flow"
+OPT_UID = "UID"
+OPT_OFFSET = "offset"
+OPT_LENGTH = "length"
+OPT_MINIMUM = "minimum"
+OPT_MAXIMUM = "minimum"
+
+# Variable defaults
+OPT_VENTILATOR_FLOW_MINIMUM = 80
+OPT_VENTILATOR_FLOW_MAXIMUM = 200
