548592eb6f3f271482991f4550fd7a19ab5acc4b
Julian Coleman <jdc@coris.org.uk>
2025-11-05 19:37:25 +0100

    Update coordinator to fetch data. Do not additionally store config data values.

--- custom_components/domat_sscp/coordinator.py
+++ custom_components/domat_sscp/coordinator.py
@@ -25,6 +25,7 @@ from .const import (
     DOMAIN,
 )
 from .sscp_connection import sscp_connection
+from .sscp_variable import sscp_variable
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,62 +36,105 @@ type DomatSSCPConfigEntry = ConfigEntry[list[DomatSSCPCoordinator]]
 class DomatSSCPCoordinator(DataUpdateCoordinator):
     """A co-ordinator to manage fetching SSCP data."""
 
-    data: list[dict[str, Any]]
+    data: dict[str, Any]
+    config_entry: DomatSSCPConfigEntry
 
-    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    def __init__(self, hass: HomeAssistant, config_entry: DomatSSCPConfigEntry) -> None:
         """Initialize coordinator."""
-
-        # From config flow
-        self.connection_name = config_entry.data[CONF_CONNECTION_NAME]
-        self.ip_address = config_entry.data[CONF_IP_ADDRESS]
-        self.port = config_entry.data[CONF_PORT]
-        self.username = config_entry.data[CONF_USERNAME]
-        self.password = config_entry.data[CONF_PASSWORD]
-        self.sscp_address = config_entry.data[CONF_SSCP_ADDRESS]
-
-        # From options
-        self.poll_interval = config_entry.options.get(
-            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-        )
 
         super().__init__(
             hass,
             logger=_LOGGER,
             config_entry=config_entry,
             name=f"{DOMAIN} {config_entry.data[CONF_CONNECTION_NAME]} ({config_entry.unique_id})",
-            update_interval=timedelta(seconds=self.poll_interval),
+            update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
         )
 
     async def _async_update_data(self):
         """Fetch data from the server and convert it to the right format."""
 
-        _LOGGER.error("Updating %s", self.connection_name)
+        # Data should not be empty, so set at least one entry
+        data: dict[str, Any] = {"connection": self.config_entry.unique_id}
+
+        conf_data = self.config_entry.data.copy()
+        conf_data["password"] = "********"
+        _LOGGER.error("Updating data %s", self.name)
+        _LOGGER.debug("Data: %s", conf_data)
+        _LOGGER.error("Options: %s", self.config_entry.options)
+
+        # TODO Retrieve entities from er
+        # Are there any variables to fetch?
+        if len(self.config_entry.options) == 0:
+            _LOGGER.error("No variables to fetch")
+            return data
+
         try:
             conn = sscp_connection(
-                name=self.connection_name,
-                ip_address=self.ip_address,
-                port=self.port,
-                user_name=self.username,
-                password=self.password,
-                sscp_address=self.sscp_address,
+                name=self.config_entry.data[CONF_CONNECTION_NAME],
+                ip_address=self.config_entry.data[CONF_IP_ADDRESS],
+                port=self.config_entry.data[CONF_PORT],
+                user_name=self.config_entry.data[CONF_USERNAME],
+                password=self.config_entry.data[CONF_PASSWORD],
+                sscp_address=self.config_entry.data[CONF_SSCP_ADDRESS],
             )
         except ValueError as error:
-            _LOGGER.error("Could not create a connection object")
+            _LOGGER.error("Could not create a connection for %s", self.name)
             raise UpdateFailed from error
 
         try:
             await conn.login()
             if conn.socket is None:
-                _LOGGER.debug("Login failed")
+                _LOGGER.error("Login failed for %s", self.name)
                 raise ConfigEntryAuthFailed from None
         except TimeoutError:
-            _LOGGER.debug("Login timeout")
+            _LOGGER.error("Login timeout for %s", self.name)
             raise ConfigEntryAuthFailed from None
         except (ValueError, OSError):
-            _LOGGER.debug("Login connection error")
+            _LOGGER.error("Login connection error for %s", self.name)
             raise UpdateFailed from None
 
-        data = ["dummy", "dummy"]
+        # Fetch variables data
+        sscp_vars: list[sscp_variable] = []
+        sscp_vars.extend(
+            sscp_variable(
+                uid=self.config_entry.options[opt_var]["uid"],
+                offset=self.config_entry.options[opt_var]["offset"],
+                length=self.config_entry.options[opt_var]["length"],
+                type=self.config_entry.options[opt_var]["type"],
+            )
+            for opt_var in self.config_entry.options
+        )
+        try:
+            error_vars, _error_codes = await conn.sscp_read_variables(sscp_vars)
+        except TimeoutError:
+            _LOGGER.error("Get sscp_var timeout for %s", self.name)
+            raise UpdateFailed from None
+        except (ValueError, OSError):
+            _LOGGER.error("Get variables failed for %s", self.name)
+            raise UpdateFailed from None
+        finally:
+            await conn.logout()
 
-        await conn.logout()
+        if len(error_vars) > 0:
+            _LOGGER.error("Variable errors for %s: %s", self.name, error_vars)
+            raise UpdateFailed from None
+
+        # Update variables with converted data
+        for sscp_var in sscp_vars:
+            _LOGGER.debug(
+                "Updated %d to 0x%s (%s)",
+                sscp_var.uid,
+                sscp_var.raw.hex(),
+                sscp_var.val,
+            )
+            # Our entity ID's are uid-length-offset of the variable
+            entity_id = (
+                str(sscp_var.uid)
+                + "-"
+                + str(sscp_var.offset)
+                + "-"
+                + str(sscp_var.length)
+            )
+            data[entity_id] = sscp_var.val
+
         return data
