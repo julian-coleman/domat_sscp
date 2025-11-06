22acaccfe05de837aed4ca4f5f2f9e64337177ff
Julian Coleman <jdc@coris.org.uk>
2025-11-06 21:52:35 +0100

    Separate value updating.  Update logging.

--- custom_components/domat_sscp/sensor.py
+++ custom_components/domat_sscp/sensor.py
@@ -31,12 +31,12 @@ async def async_setup_entry(
     """Set up the Sensors from option entries."""
 
     coordinator: DomatSSCPCoordinator = config_entry.coordinator
-    _LOGGER.debug("Sensor setup coordinator: %s", coordinator.name)
-    _LOGGER.error("Sensor setup options: %s", config_entry.options)
-    _LOGGER.debug("Sensor setup values: %s", coordinator.data)
+    _LOGGER.debug("Setup coordinator: %s", coordinator.name)
+    _LOGGER.debug("Setup options: %s", config_entry.options)
+    _LOGGER.debug("Setup values: %s", coordinator.data)
 
-    # TODO: Retrieve entities from er
     # Add sensors (class) with their initialisation data
+    # TODO: Skip sensors that are already in er
     sensors: list[SensorEntity] = []
     for opt in config_entry.options:
         if (
@@ -70,41 +70,37 @@ class DomatSSCPSensor(CoordinatorEntity, SensorEntity):
 
         super().__init__(coordinator)
 
-        # Out unique values
+        # Entity-specific values
         self.coordinator = coordinator
-        _LOGGER.error("Initialising %s with: %s", entity_id, entity_data)
         self._attr_unique_id = entity_id
         # self._attr_name = entity_data["name"]
         self._attr_native_unit_of_measurement = entity_data["unit"]
         self._attr_device_class = entity_data["class"]
         self._attr_suggested_display_precision = entity_data.get("precision", 0)
+
+        self.value = self._update_value()
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entity_data.get("device"))},
             name=entity_data.get("device"),
+            manufacturer="Domat",
+            model="SSCP Device",
         )
+        _LOGGER.error("Initialised new %s with: %s", entity_id, entity_data)
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
 
-        if self.unique_id not in self.coordinator.data:
-            _LOGGER.error("No data for %s", self.unique_id)
+        self.value = self._update_value()
         self.async_write_ha_state()
+
+    # TODO: Add a callback for when the entity is disabled/enabled and update runtime
 
     @property
     def native_value(self) -> float | None:
         """Return the current value."""
 
-        # Retrieve our value from the coordinator
-        if self.unique_id not in self.coordinator.data:
-            return None
-        value = self.coordinator.data[self.unique_id]
-
-        if self.suggested_display_precision is not None:
-            value = round(value, self.suggested_display_precision)
-        else:
-            value = round(value)
-        return value
+        return self.value
 
     @property
     def available(self) -> bool:
@@ -113,3 +109,17 @@ class DomatSSCPSensor(CoordinatorEntity, SensorEntity):
         if self.unique_id in self.coordinator.data:
             return True
         return False
+
+    def _update_value(self) -> float | None:
+        """Retrieve our value from the co-ordinator."""
+
+        if self.unique_id not in self.coordinator.data:
+            _LOGGER.error("No co-ordinator data for %s", self.unique_id)
+            return None
+
+        new_value = self.coordinator.data[self.unique_id]
+        if self.suggested_display_precision is not None:
+            new_value = round(new_value, self.suggested_display_precision)
+        else:
+            new_value = round(new_value)
+        return new_value
