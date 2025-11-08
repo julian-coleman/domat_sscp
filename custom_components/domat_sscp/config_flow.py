4a3a5c0ca9125b4c41f0cfd0e495fb83c97290f7
Julian Coleman <jdc@coris.org.uk>
2025-11-08 16:40:44 +0100

    Move helpers to the end of the config flow.

--- custom_components/domat_sscp/config_flow.py
+++ custom_components/domat_sscp/config_flow.py
@@ -92,205 +92,6 @@ _ADDR_SELECTOR = vol.All(
 _DEVICE_MENU = ["temp_hum", "energy", "air"]
 
 
-# Helpers
-def get_user_schema(
-    input_data: dict[str, Any] | None = None,
-) -> vol.Schema:
-    """Return a config flow schema with defaults based on the step and user input."""
-
-    # Fill in defaults from initial defaults or input
-    if input_data is None:
-        default_connection_name = ""
-        default_ip_address = ""
-        default_port = DEFAULT_SSCP_PORT
-        default_username = ""
-        default_password = ""
-        default_sscp_address = DEFAULT_SSCP_ADDRESS
-    else:
-        default_connection_name = input_data.get(CONF_CONNECTION_NAME, "")
-        default_ip_address = input_data.get(CONF_IP_ADDRESS, "")
-        default_port = input_data.get(CONF_PORT, DEFAULT_SSCP_PORT)
-        default_username = input_data.get(CONF_USERNAME, "")
-        default_password = input_data.get(CONF_PASSWORD, "")
-        default_sscp_address = input_data.get(CONF_SSCP_ADDRESS, DEFAULT_SSCP_ADDRESS)
-    return vol.Schema(
-        {
-            vol.Required(CONF_CONNECTION_NAME, default=default_connection_name): str,
-            vol.Required(CONF_IP_ADDRESS, default=default_ip_address): str,
-            vol.Required(CONF_PORT, default=default_port): _PORT_SELECTOR,
-            vol.Required(CONF_USERNAME, default=default_username): str,
-            vol.Required(CONF_PASSWORD, default=default_password): str,
-            vol.Required(
-                CONF_SSCP_ADDRESS, default=default_sscp_address
-            ): _ADDR_SELECTOR,
-            # vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
-        }
-    )
-
-
-def get_temp_hum_schema(
-    input_data: dict[str, Any] | None = None,
-) -> vol.Schema:
-    """Return a temperature/humidity flow schema with defaults based on the user input."""
-
-    # Fill in defaults from input or initial defaults
-    if input_data is None:
-        default_device = ""
-        default_temperature_uid = default_humidity_uid = 0
-        # default_temperature_name = "Temperature"
-        # default_humidity_name = "Humidity"
-    else:
-        default_device = input_data.get(OPT_DEVICE, "")
-        temperature = input_data.get(OPT_TEMPERATURE)
-        default_temperature_uid = temperature.get(OPT_UID, 0)
-        # default_temperature_name = temperature.get(OPT_NAME)
-        humidity = input_data.get(OPT_HUMIDITY)
-        default_humidity_uid = humidity.get(OPT_UID, 0)
-        # default_humidity_name = humidity.get(OPT_NAME)
-    return vol.Schema(
-        {
-            vol.Required(OPT_DEVICE, default=default_device): str,
-            vol.Required(OPT_TEMPERATURE): section(
-                vol.Schema(
-                    {
-                        # vol.Optional(OPT_NAME, default=default_temperature_name): str,
-                        vol.Optional(OPT_UID, default=default_temperature_uid): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-            vol.Required(OPT_HUMIDITY): section(
-                vol.Schema(
-                    {
-                        # vol.Optional(OPT_NAME, default=default_humidity_name): str,
-                        vol.Optional(OPT_UID, default=default_humidity_uid): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-        }
-    )
-
-
-def get_energy_schema(
-    input_data: dict[str, Any] | None = None,
-) -> vol.Schema:
-    """Return an energy flow schema with defaults based on the user input."""
-
-    # Fill in defaults from input or initial defaults
-    # Order is the same as in the app
-    default_meter_electricity_uid = 0
-    default_meter_water_cold_uid = default_meter_water_hot_uid = 0
-    default_calorimeter_hot_uid = default_calorimeter_cold_uid = 0
-    default_meter_electricity_name = "Electricity Meter"
-    default_meter_water_cold_name = "Cold Water Meter"
-    default_meter_water_hot_name = "Hot Water Meter"
-    default_calorimeter_hot_name = "Hot Calorimeter"
-    default_calorimeter_cold_name = "Cold Calorimeter"
-    if input_data is not None:
-        meter_electricity = input_data.get(OPT_METER_ELECTRICITY)
-        default_meter_electricity_uid = meter_electricity.get(
-            OPT_UID, default_meter_electricity_uid
-        )
-        default_meter_electricity_name = meter_electricity.get(
-            OPT_NAME, default_meter_electricity_name
-        )
-        meter_water_cold = input_data.get(OPT_METER_WATER_COLD)
-        default_meter_water_cold_uid = meter_water_cold.get(
-            OPT_UID, default_meter_water_cold_uid
-        )
-        default_meter_water_cold_name = meter_water_cold.get(
-            OPT_NAME, default_meter_water_cold_name
-        )
-        meter_water_hot = input_data.get(OPT_METER_WATER_HOT)
-        default_meter_water_hot_uid = meter_water_hot.get(
-            OPT_UID, default_meter_water_hot_uid
-        )
-        default_meter_water_hot_name = meter_water_hot.get(
-            OPT_NAME, default_meter_water_hot_name
-        )
-        calorimeter_hot = input_data.get(OPT_CALORIMETER_HOT)
-        default_calorimeter_hot_uid = calorimeter_hot.get(
-            OPT_UID, default_calorimeter_hot_uid
-        )
-        default_calorimeter_hot_name = calorimeter_hot.get(
-            OPT_NAME, default_calorimeter_hot_name
-        )
-        calorimeter_cold = input_data.get(OPT_CALORIMETER_COLD)
-        default_calorimeter_cold_uid = calorimeter_cold.get(
-            OPT_UID, default_calorimeter_cold_uid
-        )
-        default_calorimeter_cold_name = calorimeter_cold.get(
-            OPT_NAME, default_calorimeter_cold_name
-        )
-
-    return vol.Schema(
-        {
-            vol.Required(OPT_METER_ELECTRICITY): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            OPT_NAME, default=default_meter_electricity_name
-                        ): str,
-                        vol.Optional(
-                            OPT_UID, default=default_meter_electricity_uid
-                        ): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-            vol.Required(OPT_METER_WATER_COLD): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            OPT_NAME, default=default_meter_water_cold_name
-                        ): str,
-                        vol.Optional(
-                            OPT_UID, default=default_meter_water_cold_uid
-                        ): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-            vol.Required(OPT_METER_WATER_HOT): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            OPT_NAME, default=default_meter_water_hot_name
-                        ): str,
-                        vol.Optional(OPT_UID, default=default_meter_water_hot_uid): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-            vol.Required(OPT_CALORIMETER_HOT): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            OPT_NAME, default=default_calorimeter_hot_name
-                        ): str,
-                        vol.Optional(OPT_UID, default=default_calorimeter_hot_uid): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-            vol.Required(OPT_CALORIMETER_COLD): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            OPT_NAME, default=default_calorimeter_cold_name
-                        ): str,
-                        vol.Optional(
-                            OPT_UID, default=default_calorimeter_cold_uid
-                        ): int,
-                    }
-                ),
-                {"collapsed": False},
-            ),
-        }
-    )
-
-
 class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Domat SSCP."""
 
@@ -317,7 +118,7 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is None:
             return self.async_show_form(
                 step_id=step,
-                data_schema=get_user_schema(input_data),
+                data_schema=_get_user_schema(input_data),
                 errors=errors,
             )
 
@@ -349,7 +150,7 @@ class DomatSSCPConfigFlow(ConfigFlow, domain=DOMAIN):
         # There was some validation problem - previous input as defaults
         return self.async_show_form(
             step_id=step,
-            data_schema=get_user_schema(user_input),
+            data_schema=_get_user_schema(user_input),
             errors=errors,
         )
 
@@ -393,7 +194,7 @@ class DomatSSCPOptionsFlowHandler(OptionsFlow):
         description_placeholders: dict[str, str] = {}
         variables: list[sscp_variable] = []
         step = "temp_hum"
-        schema = get_temp_hum_schema(user_input)
+        schema = _get_temp_hum_schema(user_input)
 
         if user_input is None:
             return self.async_show_form(step_id=step, data_schema=schema, errors=errors)
@@ -517,7 +318,7 @@ class DomatSSCPOptionsFlowHandler(OptionsFlow):
         variables: list[sscp_variable] = []
         entity_ids: dict[str, str] = {}
         step = "energy"
-        schema = get_energy_schema(user_input)
+        schema = _get_energy_schema(user_input)
 
         if user_input is None:
             return self.async_show_form(step_id=step, data_schema=schema, errors=errors)
@@ -796,3 +597,202 @@ class DomatSSCPOptionsFlowHandler(OptionsFlow):
             if entity_id in self.config_entry.options:
                 variables = variables + str(entity_uid) + " "
         return {"variables": variables}
+
+
+# Helpers
+def _get_user_schema(
+    input_data: dict[str, Any] | None = None,
+) -> vol.Schema:
+    """Return a config flow schema with defaults based on the step and user input."""
+
+    # Fill in defaults from initial defaults or input
+    if input_data is None:
+        default_connection_name = ""
+        default_ip_address = ""
+        default_port = DEFAULT_SSCP_PORT
+        default_username = ""
+        default_password = ""
+        default_sscp_address = DEFAULT_SSCP_ADDRESS
+    else:
+        default_connection_name = input_data.get(CONF_CONNECTION_NAME, "")
+        default_ip_address = input_data.get(CONF_IP_ADDRESS, "")
+        default_port = input_data.get(CONF_PORT, DEFAULT_SSCP_PORT)
+        default_username = input_data.get(CONF_USERNAME, "")
+        default_password = input_data.get(CONF_PASSWORD, "")
+        default_sscp_address = input_data.get(CONF_SSCP_ADDRESS, DEFAULT_SSCP_ADDRESS)
+    return vol.Schema(
+        {
+            vol.Required(CONF_CONNECTION_NAME, default=default_connection_name): str,
+            vol.Required(CONF_IP_ADDRESS, default=default_ip_address): str,
+            vol.Required(CONF_PORT, default=default_port): _PORT_SELECTOR,
+            vol.Required(CONF_USERNAME, default=default_username): str,
+            vol.Required(CONF_PASSWORD, default=default_password): str,
+            vol.Required(
+                CONF_SSCP_ADDRESS, default=default_sscp_address
+            ): _ADDR_SELECTOR,
+            # vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
+        }
+    )
+
+
+def _get_temp_hum_schema(
+    input_data: dict[str, Any] | None = None,
+) -> vol.Schema:
+    """Return a temperature/humidity flow schema with defaults based on the user input."""
+
+    # Fill in defaults from input or initial defaults
+    if input_data is None:
+        default_device = ""
+        default_temperature_uid = default_humidity_uid = 0
+        # default_temperature_name = "Temperature"
+        # default_humidity_name = "Humidity"
+    else:
+        default_device = input_data.get(OPT_DEVICE, "")
+        temperature = input_data.get(OPT_TEMPERATURE)
+        default_temperature_uid = temperature.get(OPT_UID, 0)
+        # default_temperature_name = temperature.get(OPT_NAME)
+        humidity = input_data.get(OPT_HUMIDITY)
+        default_humidity_uid = humidity.get(OPT_UID, 0)
+        # default_humidity_name = humidity.get(OPT_NAME)
+    return vol.Schema(
+        {
+            vol.Required(OPT_DEVICE, default=default_device): str,
+            vol.Required(OPT_TEMPERATURE): section(
+                vol.Schema(
+                    {
+                        # vol.Optional(OPT_NAME, default=default_temperature_name): str,
+                        vol.Optional(OPT_UID, default=default_temperature_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_HUMIDITY): section(
+                vol.Schema(
+                    {
+                        # vol.Optional(OPT_NAME, default=default_humidity_name): str,
+                        vol.Optional(OPT_UID, default=default_humidity_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+        }
+    )
+
+
+def _get_energy_schema(
+    input_data: dict[str, Any] | None = None,
+) -> vol.Schema:
+    """Return an energy flow schema with defaults based on the user input."""
+
+    # Fill in defaults from input or initial defaults
+    # Order is the same as in the app
+    default_meter_electricity_uid = 0
+    default_meter_water_cold_uid = default_meter_water_hot_uid = 0
+    default_calorimeter_hot_uid = default_calorimeter_cold_uid = 0
+    default_meter_electricity_name = "Electricity Meter"
+    default_meter_water_cold_name = "Cold Water Meter"
+    default_meter_water_hot_name = "Hot Water Meter"
+    default_calorimeter_hot_name = "Hot Calorimeter"
+    default_calorimeter_cold_name = "Cold Calorimeter"
+    if input_data is not None:
+        meter_electricity = input_data.get(OPT_METER_ELECTRICITY)
+        default_meter_electricity_uid = meter_electricity.get(
+            OPT_UID, default_meter_electricity_uid
+        )
+        default_meter_electricity_name = meter_electricity.get(
+            OPT_NAME, default_meter_electricity_name
+        )
+        meter_water_cold = input_data.get(OPT_METER_WATER_COLD)
+        default_meter_water_cold_uid = meter_water_cold.get(
+            OPT_UID, default_meter_water_cold_uid
+        )
+        default_meter_water_cold_name = meter_water_cold.get(
+            OPT_NAME, default_meter_water_cold_name
+        )
+        meter_water_hot = input_data.get(OPT_METER_WATER_HOT)
+        default_meter_water_hot_uid = meter_water_hot.get(
+            OPT_UID, default_meter_water_hot_uid
+        )
+        default_meter_water_hot_name = meter_water_hot.get(
+            OPT_NAME, default_meter_water_hot_name
+        )
+        calorimeter_hot = input_data.get(OPT_CALORIMETER_HOT)
+        default_calorimeter_hot_uid = calorimeter_hot.get(
+            OPT_UID, default_calorimeter_hot_uid
+        )
+        default_calorimeter_hot_name = calorimeter_hot.get(
+            OPT_NAME, default_calorimeter_hot_name
+        )
+        calorimeter_cold = input_data.get(OPT_CALORIMETER_COLD)
+        default_calorimeter_cold_uid = calorimeter_cold.get(
+            OPT_UID, default_calorimeter_cold_uid
+        )
+        default_calorimeter_cold_name = calorimeter_cold.get(
+            OPT_NAME, default_calorimeter_cold_name
+        )
+
+    return vol.Schema(
+        {
+            vol.Required(OPT_METER_ELECTRICITY): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            OPT_NAME, default=default_meter_electricity_name
+                        ): str,
+                        vol.Optional(
+                            OPT_UID, default=default_meter_electricity_uid
+                        ): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_METER_WATER_COLD): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            OPT_NAME, default=default_meter_water_cold_name
+                        ): str,
+                        vol.Optional(
+                            OPT_UID, default=default_meter_water_cold_uid
+                        ): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_METER_WATER_HOT): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            OPT_NAME, default=default_meter_water_hot_name
+                        ): str,
+                        vol.Optional(OPT_UID, default=default_meter_water_hot_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_CALORIMETER_HOT): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            OPT_NAME, default=default_calorimeter_hot_name
+                        ): str,
+                        vol.Optional(OPT_UID, default=default_calorimeter_hot_uid): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+            vol.Required(OPT_CALORIMETER_COLD): section(
+                vol.Schema(
+                    {
+                        vol.Optional(
+                            OPT_NAME, default=default_calorimeter_cold_name
+                        ): str,
+                        vol.Optional(
+                            OPT_UID, default=default_calorimeter_cold_uid
+                        ): int,
+                    }
+                ),
+                {"collapsed": False},
+            ),
+        }
+    )
