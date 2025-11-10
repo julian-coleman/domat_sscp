afd740931f9c1ea69334ef60d1af4bec43acde77
Julian Coleman <jdc@coris.org.uk>
2025-11-10 17:18:11 +0100

    Add a binary_sensor based on sensor.

---
+++ custom_components/domat_sscp/binary_sensor.py
@@ -0,0 +1,122 @@
+"""Binary Sensor for the Domat SSCP integration."""
+
+import logging
+from typing import Any
+
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.const import CONF_BINARY_SENSORS
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from . import DomatSSCPConfigEntry
+from .const import DOMAIN
+from .coordinator import DomatSSCPCoordinator
+
+# The co-ordinator is used to centralise the data updates
+PARALLEL_UPDATES = 0
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: DomatSSCPConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up the Binary Sensors from option entries."""
+
+    coordinator: DomatSSCPCoordinator = config_entry.coordinator
+    _LOGGER.debug("Setup coordinator: %s", coordinator.name)
+    _LOGGER.debug("Setup options: %s", config_entry.options)
+    _LOGGER.debug("Setup values: %s", coordinator.data)
+
+    # Add binary_sensors (class) with their initialisation data
+    binary_sensors: list[BinarySensorEntity] = []
+    for opt in config_entry.options:
+        if (
+            "entity" in config_entry.options[opt]
+            and config_entry.options[opt]["entity"] == CONF_BINARY_SENSORS
+        ):
+            _LOGGER.error("Adding binary sensor %s: %s", opt, config_entry.options[opt])
+            binary_sensors.append(
+                DomatSSCPBinarySensor(
+                    coordinator=coordinator,
+                    entity_id=opt,
+                    entity_data=config_entry.options[opt],
+                )
+            )
+    async_add_entities(binary_sensors)
+
+
+class DomatSSCPBinarySensor(CoordinatorEntity, BinarySensorEntity):
+    """Binary Sensor types for SSCP, using coordinator for updates."""
+
+    _attr_has_entity_name = True
+    _attr_entity_registry_enabled_default = True
+
+    def __init__(
+        self,
+        coordinator: DomatSSCPCoordinator,
+        entity_id: str,
+        entity_data: dict[str, Any],
+    ) -> None:
+        """Initialise a binary sensor with provided data."""
+
+        super().__init__(coordinator)
+
+        # Entity-specific values
+        self.coordinator = coordinator
+        self._attr_unique_id = entity_id
+        self._attr_device_class = entity_data["class"]
+        self.on_value = entity_data["on"]
+        self.off_value = entity_data["off"]
+        self._attr_is_on = self._update_value()
+        if "name" in entity_data:
+            self._attr_name = entity_data["name"]
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, entity_data.get("device"))},
+            name=entity_data.get("device"),
+            manufacturer="Domat",
+            model="SSCP Device",
+        )
+        _LOGGER.debug("Initialised new %s with: %s", entity_id, entity_data)
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+
+        self._attr_is_on = self._update_value()
+        self.async_write_ha_state()
+
+    # TODO: Add a callback for when the entity is disabled/enabled and update runtime
+
+    @property
+    def is_on(self) -> bool:
+        """Return the current state."""
+
+        return self._attr_is_on
+
+    @property
+    def available(self) -> bool:
+        """Is state available?"""
+
+        if self.unique_id in self.coordinator.data:
+            return True
+        return False
+
+    def _update_value(self) -> bool:
+        """Retrieve our value from the co-ordinator."""
+
+        if self.unique_id not in self.coordinator.data:
+            _LOGGER.error("No co-ordinator data for %s", self.unique_id)
+            return None
+
+        new_value = self.coordinator.data[self.unique_id]
+        if new_value == self.on_value:
+            return True
+        if new_value == self.off_value:
+            return False
+        _LOGGER.error("Unexpected value: %s = %s", self._attr_unique_id, new_value)
+        return True
